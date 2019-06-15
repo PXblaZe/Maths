f0cd6f1023f3f5ec1ee803d1ab09414b3d1e127b
MrSumit <35930711+MrSumit@users.noreply.github.com>
2019-06-15 21:53:56 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,9 +1,21 @@
 from curses import *
-import time
+from curses import textpad
+def chrxp(stdscr, ):
+    curs_set(0)
+    init_pair(1, COLOR_RED, COLOR_BLUE)
 
-def test(stdscr):
-    stdscr.addstr(0, 0, 'Hello World')
+    t = "Hello World"
+    h, w = stdscr.getmaxyx()
+    x = w//2 - len(t)//2
+    y = h//2
+
+    textpad.rectangle(stdscr, 3, 3, h-3, w-3)
+
+    stdscr.attron(color_pair(1))
+    stdscr.addstr(y, x, t)
+    stdscr.attroff(color_pair(1))
+
     stdscr.refresh()
-    time.sleep(3)
+    stdscr.getch()
 
-wrapper(test)
+wrapper(chrxp)
