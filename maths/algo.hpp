4fa6e1961ed9a85880593f79c3d14ac2ff8df3d7
PXblaZe <gsumit178@gmail.com>
2022-10-25 14:10:15 +0530

    Header files.

---
+++ maths/algo.hpp
@@ -0,0 +1,14 @@
+#ifndef __ALGO_HPP__
+
+#define __ALGO_HPP__
+
+#include <vector>
+#include <complex>
+
+namespace algo {
+
+    void fft (std::vector<std::complex<double>>& a, bool invert);
+
+}
+
+#endif
