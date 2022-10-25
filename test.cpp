74aeb0388bca957d4091357fbb837732edd35707
PXblaZe <gsumit178@gmail.com>
2022-10-25 14:07:31 +0530

    Test code.

---
+++ test.cpp
@@ -0,0 +1,19 @@
+
+#include "maths/algebra.hpp"
+
+using namespace std;
+
+signed main() {
+
+    // Matrix<int> m(3, 3, 5);
+
+    Polynomial p;
+
+    // m *= m;
+
+    cout << p;
+
+    // for(double c: p.get_Coefficients()) cout << c << ' ';
+    cout << '\n';
+
+}
