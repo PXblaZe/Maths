2331e3f5166c958c05f144583984cd0360cfe393
PXblaZe <gsumit178@gmail.com>
2022-10-26 23:01:26 +0530

    Added some implementation to Function template.

--- maths/tools.hpp
+++ maths/tools.hpp
@@ -1,5 +1,19 @@
+#include <iostream>
 
-
+template<class CLS>
 class Function {
     
+    const CLS* funcls;
+
+    public:
+
+    Function(const CLS& callable) {
+        this->funcls = &callable;
+    }
+
+    template<typename... Type>
+    auto operator()(Type... args) const {
+        return this->funcls->operator()(args...);
+    }
+
 };
