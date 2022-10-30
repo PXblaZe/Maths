44cea780a40b5ea5fa28c5c5e5f05582527373d7
PXblaZe <gsumit178@gmail.com>
2022-10-30 19:52:48 +0530

    Added new funtionality to add methods not just callbacks.

--- maths/tools.hpp
+++ maths/tools.hpp
@@ -1,9 +1,32 @@
+#ifndef __MATHS_TOOLS_HPP__
+#define __MATHS_TOOLS_HPP__
+
 #include <iostream>
+
+template<class> class FnClsGtr;
+template<class R, class... Param> 
+class FnClsGtr<R(Param...)> {
+
+    R (*calls)(Param...) = NULL;// = (R(*)(Param...))malloc(sizeof(R(*)(Param...)));
+
+
+    public:
+    FnClsGtr(R (*callback)(Param...)) {
+        this->calls = callback;
+        std::cout << std::flush;
+    }
+
+    auto operator()(Param... args) const {
+        return (calls)(args...);
+    }
+
+};
+
 
 template<class CLS>
 class Function {
     
-    const CLS* funcls;
+    const CLS* funcls = nullptr;// = (CLS*)malloc(sizeof(CLS));
 
     public:
 
@@ -17,3 +40,5 @@ class Function {
     }
 
 };
+
+#endif
